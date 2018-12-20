16589429da7072475f912ca18b4adddabb370824
Rod Hall <rodhall@hotmail.co.uk>
2018-12-20 11:05:07 +0000

    Add Snowflakes

---
+++ Assets/Scripts/Snowflake.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snowflake : MonoBehaviour {
+
+	private float _size;
+	private float _speed;
+
+	// Use this for initialization
+	void Awake () {
+		_size = Random.Range(0.1f, 0.5f);
+		_speed = Random.Range(0.5f, 1.5f);
+	}
+
+	void Start() {
+		transform.position = new Vector3(Random.Range(-14.9f, 14.9f), 7, -2);
+		transform.localScale = new Vector3(_size, _size, 1);
+	}
+	
+	void FixedUpdate()
+	{
+		transform.position -= transform.up * Time.deltaTime * _speed;
+		if (transform.position.y < -10) {
+			Destroy(gameObject);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/SnowflakeManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowflakeManager : MonoBehaviour {
+
+	public GameObject Snowflake;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void FixedUpdate()
+	{
+		if (Time.frameCount % 15 == 0) {
+			//spawn new snowflake
+			var snowflake = new Snowflake();
+			Instantiate(Snowflake);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
