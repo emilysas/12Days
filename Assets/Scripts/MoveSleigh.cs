5d9c793950e057007242f2df5c4e3a673d54ef99
David Jacobs <david.jacobs@comparethemarket.com>
2018-12-20 12:36:17 +0000

    Added config settings class for left/right boundary settngs.

--- Assets/Scripts/MoveSleigh.cs
+++ Assets/Scripts/MoveSleigh.cs
@@ -13,18 +13,16 @@ public class MoveSleigh : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        double leftBouundary = -7.791639;
-        double rightBoundary = 7.989859;
         if(Input.GetKey(KeyCode.RightArrow)) {
             Debug.Log("Right arrow: " + transform.position.x);
-            if(transform.position.x <= rightBoundary) {
+            if(transform.position.x <= Settings.RightBoundary) {
                 transform.Translate(Vector3.right * Time.deltaTime * 10);
                 //transform.Rotate(Vector3.left * Time.deltaTime * 15);
             }
         }
         if(Input.GetKey(KeyCode.LeftArrow)) {
             Debug.Log("Left arrow: " + transform.position.x);
-            if(transform.position.x >= leftBouundary) {
+            if(transform.position.x >= Settings.LeftBouundary) {
                 transform.Translate(Vector3.left * Time.deltaTime * 10);
                 //transform.Rotate(Vector3.right * Time.deltaTime * 15);
             }
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,5 @@
+public static class Settings 
+{    
+    public static double LeftBouundary = -7.791639;
+    public static double RightBoundary = 7.989859;
+}
